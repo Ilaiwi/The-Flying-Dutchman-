1f99d2b23baf3af43a441e387f6efcc99a8904c6
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2016-01-14 00:17:25 +0100

    a

--- customer.js
+++ customer.js
@@ -26,6 +26,37 @@ $(document).ready(function () {
     var username = $.getQuery('username');
     var password = $.getQuery('password');
 
+
+    function beerData(){
+        beerId=[141001,197302,156503,604504,693502,150103,1191303,159103];
+        beerObjects=[];
+        for (i=0 ; i< beerId.length;i++){
+            $.ajax({
+                url: "http://pub.jamaica-inn.net/fpdb/api.php?username=" + username + "&password=" + password + "&action=beer_data_get&beer_id=" + beerId[i],
+                async: false,
+                cache: true,
+                success: function(data, status){
+                    beerObjects.push(data.payload[0]);
+                }
+            });
+        }
+        return beerObjects;
+    }
+
+    frontPageBeers=beerData();
+    for (i=0 ;i<frontPageBeers.length;i++){
+        if(i==4){
+            $('#beers').append("<div class='clear'>");
+        }
+        $('#beers').append("<div class=item data-price="+ frontPageBeers[i].prisinklmoms +" data-beerid="+ frontPageBeers[i].nr +" >"
+            +"<img src='./beer/"+frontPageBeers[i].namn +".jpg' />"+"<BR CLEAR=ALL>"+frontPageBeers[i].namn+
+        "</div>");
+        //console.log("<div class=item data-price="+ frontPageBeers[i].prisinklmoms +" data-beerid="+ frontPageBeers[i].nr +" >"
+        //    +"<img src='./beer/"+frontPageBeers[i].namn +".jpg' />"+
+        //    "</div>");
+
+    }
+
     //$.get(
     //    url :"http://pub.jamaica-inn.net/fpdb/api.php?username=" + username + "&password=" + password + "&action=iou_get",
     //    async : false,
@@ -73,12 +104,29 @@ $(document).ready(function () {
     });
 
 
+
+
     $(".quantity").droppable({
         drop: function (event, ui) {
             var price = ui.draggable.attr('data-price');
             var beerid = ui.draggable.attr('data-beerid');
             console.log(this);
             $("#orders").append('<div class="inCart floatLeft">' + "<div>" + ui.draggable.html() + "</div>" + "<div>" + "total price=" + price * parseInt($(this).html()) + "</div>" + "</div>");
+            $(".inCart").draggable({
+                helper: 'clone',
+                drag: function () {
+                }
+            });
+        }
+    });
+
+    $("#trash").droppable({
+        drop: function (event, ui) {
+            ui.draggable.remove();
+            $('#trash').css({"background": "none"})
+        },
+        over: function(event,ui){
+            $('#trash').css({"background-color": "cadetblue","alpha": "0.3","border-radius": "10px"});
         }
     });
 
@@ -94,6 +142,9 @@ function hide(div) {
 document.onkeydown = function (evt) {
     evt = evt || window.event;
     if (evt.keyCode == 27) {
+        hide('popup');
         hide('popDiv');
     }
 };
+
+
